2b25582ab91decf74d34f5804c5d4a48e08311d6
Derry Livensky <code@derryspann.com>
2025-01-01 20:27:24 -0500

    added legacy pyonir logic and scaffolding

--- setup.py
+++ setup.py
@@ -8,7 +8,13 @@ setup(
     author_email='pyonir@derryspann.com',
     version='0.0.1',
     packages=find_packages(),
+    package_data={
+        'pyonir': ['libs/*']
+    },
+    include_package_data=True,
     entry_points={
-        "console_scripts": ["pyonir-start = pyonir:cli.PyonirSetup"]
+        "console_scripts": [
+            "pyonir-create = pyonir:cli.PyonirSetup"
+        ]
     }
 )

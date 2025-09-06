bd119e37df6c01e4ae0084557ae1a8e8eb86fa99
Derry Livensky <code@derryspann.com>
2025-09-06 09:46:16 -0400

    update media manager docs and return types after uploads

--- pyonir/models/page.py
+++ pyonir/models/page.py
@@ -1,10 +1,13 @@
 import base64
+import re
 from datetime import datetime
 import os, pytz
 from pathlib import Path
-from typing import Any
+from typing import Any, Union, Optional
 
-from pyonir.utilities import deserialize_datestr
+from pyonir.models.database import BasePagination
+from pyonir.models.parser import DeserializeFile
+from pyonir.utilities import get_attr
 
 IMG_FILENAME_DELIM = '::'  # delimits the file name and description
 
@@ -22,37 +25,31 @@ class PageStatus(str):
     """Access external and internal with READ and WRITE."""
 
 
-class BasePage:
-    """Represents a single page returned from a web request"""
-    content: str = None
-    template: str = None
-    status: str = PageStatus.PUBLIC
+class BaseFile:
+    """Represents a single file on file system"""
 
-    def __init__(self, path: str = None):
+    def __init__(self, path: str = None, contents_dirpath: str = None):
         self.file_path = str(path or __file__)
-        name, ext = os.path.splitext(os.path.basename(self.file_path))
-        self.file_name = name
-        self.file_ext = ext[1:]
-        self.file_dirpath = os.path.dirname(self.file_path)
-        self.file_dirname = os.path.basename(os.path.dirname(self.file_path))
-        relpath = os.path.relpath(self.file_path)
+        self.file_ext = os.path.splitext(self.file_path)[1]
 
+    @property
+    def file_name(self):
+        name, ext = os.path.splitext(os.path.basename(self.file_path))
+        self.file_ext = ext
+        return name
+
+    @property
+    def file_dirpath(self):
+        return os.path.dirname(self.file_path)
+
+    @property
+    def file_dirname(self):
+        return os.path.basename(os.path.dirname(self.file_path))
 
     @property
     def file_status(self) -> str:  # String
         return PageStatus.PROTECTED if self.file_name.startswith('_') else \
             PageStatus.FORBIDDEN if self.file_name.startswith('.') else PageStatus.PUBLIC
-
-    @property
-    def url(self): return f'/{self.slug}'
-
-    @property
-    def slug(self): return
-
-    @property
-    def canonical(self):
-        from pyonir import Site
-        return f"{Site.domain}{self.url}" if Site else self.url
 
     @property
     def created_on(self):  # Datetime
@@ -65,133 +62,165 @@ class BasePage:
     def to_dict(self) -> dict[str, Any]:
         return self.__dict__
 
+# @dataclass
+class BasePage:
+    """Represents a single page returned from a web request"""
+    _orm_options = {"mapper": {'created_on': 'file_created_on', 'modified_on': 'file_modified_on'}}
+    url: str = ''
+    slug: str = ''
+    title: str = ''
+    content: str = ''
+    template: str = 'pages.html'
+    created_on: datetime = None
+    modified_on: datetime = None
+    entries: BasePagination = None
 
-class BaseMedia(BasePage):
-    """Represents an image file and its file captions.
-    Caption are extracted from the file name {name}|{caption}--{width}x{height}.{ext}"""
+    def __lt__(self, other) -> bool:
+        """Compares two BasePage instances based on their created_on attribute."""
+        if not isinstance(other, BasePage):
+            return True
+        return self.created_on < other.created_on
 
-    def __init__(self, path: str = None):
-        super().__init__(path)
-        self.width = None
-        self.height = None
-        self.thumbnails = dict()
-        self.meta = self.decode_filename(self.file_name)
-        self.open_image()
-        pass
-
-    @property
-    def slug(self):
-        return f"{self.file_dirname}/{self.file_name}.{self.file_ext}"
-
-    def open_image(self, file_path: str = None):
-        """Opens selected image into memory to retrieve dimensions"""
-        from PIL import Image
-        from pyonir.utilities import get_attr
-        try:
-            raw_img = Image.open(file_path or self.file_path)
-        except Exception as ie:
-            raw_img = None
-            print(f"Unable to open {self.file_path}: {ie}")
-        self.width = get_attr(raw_img, "width", None)
-        self.height = get_attr(raw_img, "height", None)
-        return raw_img
-
-    @staticmethod
-    async def save_upload(file, img_folder_abspath) -> str:
-        """Saves base64 file contents into file system"""
-        file_name, file_ext = os.path.splitext(file.filename)
-        new_dir_path = Path(img_folder_abspath)
-        new_dir_path.mkdir(parents=True, exist_ok=True)
-        new_file_path = os.path.join(img_folder_abspath, file_name + file_ext)
-        file_contents = await file.read()
-        if not file_contents: return ''
-        with open(str(new_file_path), 'wb') as f:
-            f.write(file_contents)
-        return new_file_path
-
-    @staticmethod
-    def get_image_dimensions(file_path: str):
-        """Opens selected image into memory to retrieve dimensions"""
-        from PIL import Image
-        from pyonir.utilities import get_attr
-        try:
-            raw_img = Image.open(file_path)
-        except Exception as ie:
-            raw_img = None
-            print(f"Unable to open {file_path}: {ie}")
-        width = get_attr(raw_img, "width", None)
-        height = get_attr(raw_img, "height", None)
-        return width, height
-
-    @staticmethod
-    def get_media_dimensions(media_file_path: str):
-        from pymediainfo import MediaInfo
-        media_info = MediaInfo.parse(media_file_path)
-        for track in media_info.tracks:
-            if track.track_type == "Image":
-                return {
-                    "format": track.format,
-                    "width": track.width,
-                    "height": track.height,
-                    "bit_depth": getattr(track, "bit_depth", None),
-                    "compression_mode": getattr(track, "compression_mode", None),
-                    "color_space": getattr(track, "color_space", None),
-                }
-            if track.track_type == "Audio":
-                return {
-                    "codec": track.codec,
-                    "duration": track.duration / 1000 if track.duration else None,  # ms → seconds
-                    "bit_rate": track.bit_rate,
-                    "channels": track.channel_s,
-                    "sampling_rate": track.sampling_rate,
-                }
-            if track.track_type == "Video":
-                return {
-                    "codec": track.codec,
-                    "duration": track.duration / 1000 if track.duration else None,  # ms → seconds
-                    "width": track.width,
-                    "height": track.height,
-                    "frame_rate": track.frame_rate,
-                    "bit_rate": track.bit_rate,
-                }
-
-    @staticmethod
-    def encode_filename(name: str, caption: str = ' ') -> str:
-        """
-        Build filename as {name}::{caption}::{created_date},
-        then Base64 encode (URL-safe, no '.' in output).
-        """
-        name, ext = os.path.splitext(name)
-        created_date = int(datetime.now().timestamp())
-        raw = f"{name}{IMG_FILENAME_DELIM}{caption}{IMG_FILENAME_DELIM}{created_date}"
-
-        # URL-safe base64 (no + or /), strip padding '='
-        b64 = base64.urlsafe_b64encode(raw.encode()).decode().rstrip("=")
-
-        return b64+ext
-
-    @staticmethod
-    def decode_filename(encoded: str):
-        """
-        Reverse of encode_filename.
-        """
-        import re
-        # restore padding
-        padding = "=" * (-len(encoded) % 4)
-        encoded = encoded.replace("_", ".") + padding
-        raw = base64.urlsafe_b64decode(encoded.encode()).decode()
-
-        # split into parts
-        name_caption, ext = os.path.splitext(raw)
-        parts = name_caption.split(IMG_FILENAME_DELIM)
-        if len(parts) != 3:
-            raise ValueError(f"Invalid encoded filename: {raw}")
-
-        name, caption, created_date = parts
-        display_name = re.sub(r'[^a-zA-Z0-9]+', ' ', name).title() # initial cap name
-        return {
-            "name": name,
-            "display_name": display_name,
-            "caption": caption,
-            "created_date": deserialize_datestr(datetime.fromtimestamp(int(created_date))),
-        }
+#
+# class BaseMedia(BaseFile):
+#     """Represents an image file and its details."""
+#
+#     def __init__(self, path: str = None):
+#         super().__init__(path)
+#         filename_meta = self.decode_filename(self.file_name)
+#         self.data = filename_meta or self.get_media_data(self.file_path)
+#         self.has_encoded_filename = bool(filename_meta)
+#         pass
+#
+#     @property
+#     def file_name(self):
+#         name, ext = os.path.splitext(os.path.basename(self.file_path))
+#         self.file_ext = ext
+#         return name
+#
+#     @property
+#     def file_dirpath(self):
+#         return os.path.dirname(self.file_path)
+#
+#     @property
+#     def file_dirname(self):
+#         return os.path.basename(os.path.dirname(self.file_path))
+#
+#     @property
+#     def file_status(self) -> str:  # String
+#         return PageStatus.PROTECTED if self.file_name.startswith('_') else \
+#             PageStatus.FORBIDDEN if self.file_name.startswith('.') else PageStatus.PUBLIC
+#
+#     @property
+#     def slug(self):
+#         return f"{self.file_dirname}/{self.file_name}{self.file_ext}"
+#
+#     def rename_media_file(self) -> None:
+#         """Renames media file as b64 encoded value"""
+#         encoded_filename = self.encode_filename(self.file_path, self.data)
+#         new_filepath = self.file_path.replace(self.file_name+self.file_ext, encoded_filename)
+#         os.rename(self.file_path, new_filepath)
+#         self.file_path = new_filepath
+#
+#     def open_image(self):
+#         from PIL import Image
+#         raw_img = Image.open(self.file_path)
+#         return raw_img
+#
+#     def resize(self, sizes=None):
+#         '''
+#         Resize each image and save to the upload path in corresponding image size and paths
+#         This happens after full size images are saved to the filesystem
+#         '''
+#         from PIL import Image
+#         from pyonir import Site
+#         raw_img = self.open_image()
+#         if sizes is None:
+#             sizes = [Site.THUMBNAIL_DEFAULT]
+#         try:
+#             for dimensions in sizes:
+#                 width, height = dimensions
+#                 # self._sizes.append(dimensions)
+#                 img = raw_img.resize((width, height), Image.Resampling.BICUBIC)
+#                 file_name = f'{self.file_name}--{width}x{height}'
+#                 img_dirpath = os.path.dirname(self.file_path)
+#                 Path(img_dirpath).mkdir(parents=True, exist_ok=True)
+#                 filepath = os.path.join(img_dirpath, Site.UPLOADS_THUMBNAIL_DIRNAME, file_name + '.' + self.file_ext)
+#                 if not os.path.exists(filepath): img.save(filepath)
+#         except Exception as e:
+#             raise
+#
+#     @staticmethod
+#     def decode_filename(encoded_filename: str) -> Optional[dict]:
+#         """ Reverse of encode_filename. """
+#         from urllib.parse import parse_qs
+#
+#         try:
+#             # restore padding
+#             padding = "=" * (-len(encoded_filename) % 4)
+#             encoded_filename = encoded_filename.replace("_", ".") + padding
+#             raw = base64.urlsafe_b64decode(encoded_filename.encode()).decode()
+#             parsed = parse_qs(raw)
+#             return parsed
+#         except Exception as e:
+#             return None
+#
+#     @staticmethod
+#     def encode_filename(file_path: str, meta_data: dict = None) -> str:
+#         """
+#         Build filename as url encoded string, then Base64 encode (URL-safe, no '.' in output).
+#         """
+#         from urllib.parse import urlencode
+#
+#         file_name, file_ext = os.path.splitext(os.path.basename(file_path))
+#         created_date = int(datetime.now().timestamp())
+#         raw = urlencode(meta_data) if meta_data else f'name={file_name}&ext={file_ext}&created_date={created_date}'
+#         # URL-safe base64 (no + or /), strip padding '='
+#         b64 = base64.urlsafe_b64encode(raw.encode()).decode().rstrip("=")
+#         return b64+file_ext
+#
+#     @staticmethod
+#     async def save_upload(file, img_folder_abspath) -> str:
+#         """Saves base64 file contents into file system"""
+#         file_name, file_ext = os.path.splitext(file.filename)
+#         new_dir_path = Path(img_folder_abspath)
+#         new_dir_path.mkdir(parents=True, exist_ok=True)
+#         new_file_path = os.path.join(img_folder_abspath, file_name + file_ext)
+#         file_contents = await file.read()
+#         if not file_contents: return ''
+#         with open(str(new_file_path), 'wb') as f:
+#             f.write(file_contents)
+#         return new_file_path
+#
+#     @staticmethod
+#     def get_media_data(media_file_path: str):
+#         from pymediainfo import MediaInfo
+#         media_info = MediaInfo.parse(media_file_path)
+#         media_track_file = media_info.tracks.pop(0)
+#         created_on = media_track_file.file_creation_date
+#         for track in media_info.tracks:
+#             if track.track_type == "Image":
+#                 return {
+#                     "name": media_track_file.file_name,
+#                     "created_on": created_on,
+#                     "width": track.width,
+#                     "height": track.height,
+#                 }
+#             if track.track_type == "Audio":
+#                 dur = track.duration / 1000 if track.duration else None # ms → seconds
+#                 return {
+#                     "codec": track.codec,
+#                     "duration": dur,
+#                     "bit_rate": track.bit_rate,
+#                     "channels": track.channel_s,
+#                     "sampling_rate": track.sampling_rate,
+#                 }
+#             if track.track_type == "Video":
+#                 return {
+#                     "codec": track.codec,
+#                     "duration": track.duration / 1000 if track.duration else None,  # ms → seconds
+#                     "width": track.width,
+#                     "height": track.height,
+#                     "frame_rate": track.frame_rate,
+#                     "bit_rate": track.bit_rate,
+#                 }

09869686032037263f9e025d106ac6d56ccfd270
Derry Livensky <code@derryspann.com>
2025-12-02 22:59:15 -0500

    hardcode version to reduce runtime perf

--- pyonir/__init__.py
+++ pyonir/__init__.py
@@ -18,7 +18,7 @@ PYONIR_JINJA_TEMPLATES_DIRPATH = os.path.join(PYONIR_JINJA_DIRPATH, "templates")
 PYONIR_JINJA_EXTS_DIRPATH = os.path.join(PYONIR_JINJA_DIRPATH, "extensions")
 PYONIR_JINJA_FILTERS_DIRPATH = os.path.join(PYONIR_JINJA_DIRPATH, "filters")
 
-__version__: str = get_version(PYONIR_TOML_FILE)
+__version__: str = "0.0.53"
 Site: Optional[BaseApp] = None
 
 

ee422f3c6d68156d84e03e446436dd99d079da68
Derry Livensky <code@derryspann.com>
2026-03-31 04:37:23 -0400

    code cleanup, added cats to pages

--- pyonir/core/page.py
+++ pyonir/core/page.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from datetime import datetime
 import os, pytz
 from typing import Any
+from pathlib import Path
+from typing import Tuple, List
 
 IMG_FILENAME_DELIM = '::'  # delimits the file name and description
 
@@ -19,53 +21,58 @@ class PageStatus(str):
     """Access external and internal with READ and WRITE."""
 
 
-class BaseFile:
-    """Represents a single file on file system"""
-
-    def __init__(self, path: str = None, contents_dirpath: str = None):
-        self.file_path = str(path or __file__)
-        self.file_ext = os.path.splitext(self.file_path)[1]
-
-    @property
-    def file_name(self):
-        name, ext = os.path.splitext(os.path.basename(self.file_path))
-        self.file_ext = ext
-        return name
-
-    @property
-    def file_dirpath(self):
-        return os.path.dirname(self.file_path)
-
-    @property
-    def file_dirname(self):
-        return os.path.basename(os.path.dirname(self.file_path))
-
-    @property
-    def file_status(self) -> str:  # String
-        return PageStatus.PROTECTED if self.file_name.startswith('_') else \
-            PageStatus.FORBIDDEN if self.file_name.startswith('.') else PageStatus.PUBLIC
-
-    @property
-    def created_on(self):  # Datetime
-        return datetime.fromtimestamp(os.path.getctime(self.file_path), tz=pytz.UTC)
-
-    @property
-    def modified_on(self):  # Datetime
-        return datetime.fromtimestamp(os.path.getmtime(self.file_path), tz=pytz.UTC)
-
-    def to_dict(self) -> dict[str, Any]:
-        return self.__dict__
-
 @dataclass
 class BasePage:
     """Represents a single page returned from a web request"""
     __alias__ = {'created_on': 'file_created_on', 'modified_on': 'file_modified_on'}
     template: str = 'pages.html'
+    url: str = ''
+    slug: str = ''
     created_on: datetime = None
     modified_on: datetime = None
+    _category: str = ''
+    _subcategories: str = ''
 
     def __lt__(self, other) -> bool:
         """Compares two BasePage instances based on their created_on attribute."""
         if not isinstance(other, BasePage):
             return True
         return self.created_on < other.created_on
+
+    def __post_init__(self):
+        cat, subcats = self.parse_taxonomy(self.slug)
+        if cat:
+            self._subcategories = subcats
+            self._category = cat
+
+    @property
+    def category(self):
+        return self._category
+
+    @property
+    def subcategory(self):
+        return self._subcategories[0]
+
+    @staticmethod
+    def parse_taxonomy(path: str) -> Tuple[str, List[str]]:
+        p = Path(path)
+
+        # remove filename
+        parts = p.parts[:-1]
+
+        # remove leading slash if present
+        parts = [p for p in parts if p != "/"]
+
+        if not parts:
+            return None, []
+
+        category = parts[0]
+        subcategories = list(parts[1:])
+
+        return category, subcategories
+
+    def to_dict(self):
+        cat, sub_cat = self.parse_taxonomy(self.slug)
+        res = {"category": cat}
+        res.update(self.__dict__)
+        return res
